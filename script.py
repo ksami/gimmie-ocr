aeaae426c9479ee853407005b39ef0f0978cffe3
ksami <ksami.ihide@gmail.com>
2015-12-24 22:51:36 +0800

    add configs to tesseract to restrict scope

--- script.py
+++ script.py
@@ -4,7 +4,13 @@ from PIL import Image
 import pytesseract
 from SimpleCV import Image as SImage
 
-img = SImage('./images/receipt.jpg')
+
+# Preprocessing
+input_img = SImage('./images/receipt.jpg')
+img = input_img.blur(2).binarize(128).invert()
 img.show()
+
+
+# OCR
 pil_img = Image.fromstring('RGB', img.size(), img.toString())
-print(pytesseract.image_to_string(pil_img))
+print(pytesseract.image_to_string(pil_img, lang='eng', config='./receipt --user-words=./eng.user-words'))
