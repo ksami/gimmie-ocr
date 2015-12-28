edabae2d775098431c8d5d0050340f8090987ed6
ksami <ksami.ihide@gmail.com>
2015-12-28 19:26:34 +0800

    try fuzzy string matching

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 - tesseract 3.02
 - pytesseract 0.1.6
 - SimpleCV 1.3.0
+- fuzzywuzzy 0.8.0
--- script.py
+++ script.py
@@ -4,7 +4,7 @@ from PIL import Image
 
 import pytesseract
 from SimpleCV import Image as SImage
-
+from fuzzywuzzy import process as FProcess
 
 # Preprocessing
 input_img = SImage('./images/receipt.jpg')
@@ -16,6 +16,37 @@ img.show()
 pil_img = Image.fromstring('RGB', img.size(), img.toString())
 output = pytesseract.image_to_string(pil_img, lang='eng', config='./receipt --user-words=./eng.user-words')
 
+# Parse output
+choices = [
+    'FAIRPRICE XTRA',
+    'NEX HYPERMART',
+    'SINGAPORE',
+    'REG',
+    'GST',
+    'SENGCHOON EGG 55 G',
+    'GD S/V ENR WHT 600G',
+    'N-FRS SOYAMK ALMD1L',
+    'FP SPINACH 250G',
+    'FP PF SATAY-BEEF500',
+    'DELMONTE BANANAS-BAG',
+    'NSOY FR S/MILK A',
+    'NORMAL PRICE',
+    'LINK CARD',
+    'SUBTOTAL',
+    'TOTAL',
+    'VISA'
+]
+
+lines = output.split('\n')
+results = 'Actual | Predicted\n'
+for line in lines:
+    (predicted, ratio) = FProcess.extractOne(line, choices)
+    if ratio==0:
+        results += line + ' | \n'
+    else:
+        results += line + ' | (' + predicted + ', ' + str(ratio) + ')\n'
+
+
 filename = './results/' + datetime.now().strftime("%Y%m%d-%H%M%S") + '.txt'
 with open(filename, 'w') as f:
-    f.write(output)
+    f.write(results)
