d7441841fa8c9291d3ad99628c8427e3c68bedbf
ksami <ksami.ihide@gmail.com>
2015-12-31 15:40:37 +0800

    link c++ and python script

--- script.py
+++ script.py
@@ -1,20 +1,26 @@
 # /usr/bin/python
 from datetime import datetime
-from PIL import Image
+# from PIL import Image
 
-import pytesseract
+# import pytesseract
 from SimpleCV import Image as SImage
 from fuzzywuzzy import process as FProcess
+import subprocess
+import shlex
 
 # Preprocessing
 input_img = SImage('./images/receipt.jpg')
-img = input_img.blur(2).binarize(128).invert()
-img.show()
+img = input_img.blur(2).binarize(128).invert().scale(2)
+img.save('./processed/receipt.jpg')
 
 
 # OCR
-pil_img = Image.fromstring('RGB', img.size(), img.toString())
-output = pytesseract.image_to_string(pil_img, lang='eng', config='./receipt --user-words=./eng.user-words')
+ocr_bin = shlex.split('./ocr ./processed/receipt.jpg')
+process = subprocess.Popen(ocr_bin, shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+output, err = process.communicate()
+
+# pil_img = Image.fromstring('RGB', img.size(), img.toString())
+# output = pytesseract.image_to_string(pil_img, lang='eng', config='./receipt --user-words=./eng.user-words')
 
 # Parse output
 choices = [
@@ -38,13 +44,18 @@ choices = [
 ]
 
 lines = output.split('\n')
-results = 'Actual | Predicted\n'
+results = 'Actual | Predicted | OCR Conf\n'
 for line in lines:
-    (predicted, ratio) = FProcess.extractOne(line, choices)
-    if ratio==0:
-        results += line + ' | \n'
-    else:
-        results += line + ' | (' + predicted + ', ' + str(ratio) + ')\n'
+    if(line.startswith('line: ')):
+        ln = line[6:]
+        predicted, ratio = FProcess.extractOne(ln, choices)
+        if ratio==0:
+            results += ln + ' | '
+        else:
+            results += ln + ' | (' + predicted + ', ' + str(ratio) + ') | '
+    elif(line.startswith('conf: ')):
+        ln = line[6:]
+        results += ln + '\n'
 
 
 filename = './results/' + datetime.now().strftime("%Y%m%d-%H%M%S") + '.txt'
